a01c0f1c2a6bf5c7730cd1ad110cc1c43f0b6835
Vulpes <fccs1998@gmail.com>
2021-10-15 01:11:11 -0300

    Alterações que não deram certo

--- Assets/Scripts/RotatePrefab.cs
+++ Assets/Scripts/RotatePrefab.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RotatePrefab : MonoBehaviour
 {
+    [SerializeField]
+    private Button virarMesa;
+    [SerializeField]
+    private Text help;
+    [SerializeField]
+    private GameObject mesa;
+
+
     void Start()
     {
-        
+        virarMesa.onClick.AddListener(VirarAMesa);
     }
 
-    // Update is called once per frame
+    void VirarAMesa(){
+        var lookPos = mesa.transform.position;
+        lookPos.x = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        rotation *= Quaternion.Euler(-90, 0, 0); // this adds a 90 degrees Y rotation
+
+        mesa.transform.rotation = rotation;
+
+    }
+
     void Update()
     {
-        
+        help.text = "UM CU: " + mesa.transform.rotation;
     }
 }
